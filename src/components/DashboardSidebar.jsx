ff8a21bc270f7941b9ec0df6f46e56dc0a8ddc34
rinklepoonia <rinklepoonia889@gmail.com>
2025-03-28 15:00:38 +0530

    push router

--- src/components/DashboardSidebar.jsx
+++ src/components/DashboardSidebar.jsx
@@ -1,16 +1,20 @@
+"use client";
 import Image from 'next/image'
 import React from 'react'
 import Icons from './common/ui/Icons'
 import { DASHBOARD_ICON_DATA_LIST } from '@/app/lib/helper'
+import { useRouter } from "next/navigation";
 
 const DashboardSidebar = () => {
+  const router = useRouter();
+
   return (
     <div>
       <Image width={58} height={34} src="/assets/images/png/page-logo.png" alt='logo' />
       <div className='border border-solid border-[#0000001A] w-[60px] mt-4 mb-6'></div>
       <div className='flex flex-col gap-4.5'>
         {DASHBOARD_ICON_DATA_LIST.map((obj, i) => (
-          <div key={i}>
+          <div key={i} onClick={() => router.push(obj.path)}>
             <div className='size-11.5 bg-blue-gradient rounded-sm flex justify-center items-center mx-auto'>
               <Icons className='size-[29px]' icon={obj.icon} />
             </div>
